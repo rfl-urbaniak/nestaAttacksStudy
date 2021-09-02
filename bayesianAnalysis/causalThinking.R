edecc65bf8f1027ea3e553840f186fd3e1bac382
rfl-urbaniak <rfl-urbaniak@github.com>
2021-09-02 08:32:28 +0200

    started bayesian analysis, ignore datasets

--- bayesianAnalysis/causalThinking.R
+++ bayesianAnalysis/causalThinking.R
@@ -7,89 +7,121 @@ library(rethinking)
 #B, D, A: before, during, after
 #T, C: type, count
 
-
-large <- dagitty("
+dag <- dagitty("
     dag{
-    AB -> AD -> AA
-    AB -> HB -> HD -> HA
-    AD -> HD
-    AA -> HA
-    IT -> HD
-    IT -> AD
-    IT -> AA
-    IT -> HA
-    IC -> HD
-    IC -> AD
-    IC -> AA
-    IC -> HA
-    } 
-  ")
+        AD -> HD -> IC
+        U [unobserved]
+        U -> AD
+        U -> HD
+        U -> AC
+        U -> HC
+        AC -> HC
+        IC -> AC
+        IC -> HC
+        IT -> AC
+        IT -> HC
+        }")
+
+
+drawdag(dag)
+
+
+paths(dag, from = c("IC"), to = "HC")
 
 
 
-drawdag(large)
-
-
-adjustmentSets(large, exposure = c("IC", "IT"), outcome = "HA")
-
-adjustmentSets(large, exposure = c("IC", "IT"), outcome = "HA", effect = "direct")
+paths(dag, from = c("IC"), to = "AC")
 
 
 
-impliedConditionalIndependencies(large)
+impliedConditionalIndependencies(dag)
 
+adjustmentSets(dag, exposure = c("IC", "IT"), outcome = "HC")
 
-
-small <- dagitty("
-    dag{
-    AB ->  AA
-    AB -> HB -> HA
-    AA -> HA
-    I -> AA
-    I -> HA
-    } 
-  ")
-
-drawdag(small)
-
-adjustmentSets(small, exposure = c("I"), outcome = "HA")
-
-
-adjustmentSets(small, exposure = c("I"), outcome = "HA", effect = "direct")
+adjustmentSets(dag, exposure = c("IC", "IT"), outcome = "AC")
 
 
 
 
-impliedConditionalIndependencies(large)
-
-
-hidden <- dagitty("
-    dag{
-    UA [unobserved]
-    UH [unobserved]
-    AB ->  HB
-    AA -> HA
-    I -> AA
-    I -> HA
-    UA -> AB
-    UA -> AA
-    UH -> HB
-    UH -> HA
-    } 
-  ")
-
-
-drawdag(hidden)
-
-adjustmentSets(hidden, exposure = c("I"), outcome = "HA", effect = "total")
-adjustmentSets(hidden, exposure = c("I"), outcome = "HA", effect = "direct")
+adjustmentSets(dag, exposure = c("IC", "IT"), outcome = "AC", effect = "direct")
+adjustmentSets(dag, exposure = c("IC"), outcome = "AC", effect = "direct")
 
 
 
-impliedConditionalIndependencies(hidden)
+adjustmentSets(dag, exposure = c("IC", "IT"), outcome = "HC", effect = "direct")
 
 
-paths(hidden, from = "I", to = "HA", limit = 100)
 
 
+
+
+length(equivalentDAGs(dag))
+
+# Plot all equivalent DAGs
+par( mfrow=c(4,4) )
+lapply( equivalentDAGs(dag), plot )
+
+# How many edges can be reversed 
+
+edges(equivalenceClass(dag2))[edges(equivalenceClass(dag))$e == "--",]
+
+
+
+
+
+dag2 <- dagitty("
+               dag{
+               AD -> HD -> IC
+               AD -> AC
+               HD  -> HC
+               U [unobserved]
+               U -> AD
+               U -> HD
+               U -> AC
+               U -> HC
+               AC -> HC
+               IC -> AC
+               IC -> HC
+               IT -> AC
+               IT -> HC
+               }")
+
+
+drawdag(dag2)
+
+
+paths(dag2, from = c("IC"), to = "HC")
+
+
+
+paths(dag2, from = c("IC"), to = "AC")
+
+
+
+impliedConditionalIndependencies(dag2)
+
+adjustmentSets(dag2, exposure = c("IC", "IT"), outcome = "HC")
+
+adjustmentSets(dag2, exposure = c("IC", "IT"), outcome = "AC")
+
+
+
+
+adjustmentSets(dag2, exposure = c("IC", "IT"), outcome = "AC", effect = "direct")
+adjustmentSets(dag2, exposure = c("IC"), outcome = "AC", effect = "direct")
+
+adjustmentSets(dag2, exposure = c("IC", "IT"), outcome = "HC", effect = "direct")
+
+
+
+
+length(equivalentDAGs(dag2))
+
+# Plot all equivalent DAGs
+par( mfrow=c(2,4) )
+lapply( equivalentDAGs(dag2), plot )
+
+# How many edges can be reversed without c
+
+edges(equivalenceClass(dag2))[edges(equivalenceClass(dag2))$e == "--",]
 
--- bayesianAnalysis/priorsSelection.R
+++ bayesianAnalysis/priorsSelection.R
@@ -7,26 +7,20 @@ sim <- seq(0,400,by = 1)
 p <- ggplot(data = data.frame(count = sim), 
             mapping = aes(x = count))+theme_tufte()
 
+xdnorm <- seq(-4,4,by = 0.01)
+pd <- ggplot(data = data.frame(change = xdnorm), 
+            mapping = aes(x = change))+theme_tufte()
 
 
-priorHA <-  function(x) dcauchy(x,1,22)
-p + stat_function(fun = priorHA)+xlim(c(0,20))+ggtitle("Prior for hate after")
+priorHD <-  function(x) dcauchy(x,1,22)
+p + stat_function(fun = priorHD)+xlim(c(0,20))+ggtitle("Prior for hate during")
 
-priorHB <-  function(x) dcauchy(x,1,22)
-p + stat_function(fun = priorHA)+xlim(c(0,20))+ggtitle("Prior for hate before")
+priorAC <- function(x) dnorm(x,0,1)
+pd + stat_function(fun = priorAC)+ggtitle("Prior for activity change")
 
+priorHC <- function(x) dnorm(x,0,1)
+pd + stat_function(fun = priorHC)+ggtitle("Prior for change in attacks")
 
-priorAB <- function(x) dcauchy(x,23,22)
-p + stat_function(fun = priorAB)+xlim(c(0,100))+ggtitle("Prior for activity before")
-
-priorAB <- function(x) dnorm(x,23,30)
-p + stat_function(fun = priorAB)+xlim(c(0,100))+ggtitle("Prior for activity before")
-
-priorAA <- function(x) dcauchy(x,23,22)
-p + stat_function(fun = priorAB)+xlim(c(0,100))+ggtitle("Prior for activity before")
-
-priorAA <- function(x) dnorm(x,23,30)
-p + stat_function(fun = priorAB)+xlim(c(0,100))+ggtitle("Prior for activity before")
 
 
 

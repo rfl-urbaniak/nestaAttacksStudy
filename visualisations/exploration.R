68d5d672ffebae2293929ebafaf2309d39c64b5d
rfl-urbaniak <rfl-urbaniak@github.com>
2021-09-03 11:59:56 +0200

    visualisations

--- visualisations/exploration.R
+++ visualisations/exploration.R
@@ -87,7 +87,10 @@ ggsave("images/CCS.pdf", width = 20, height = 15, units = "cm", dpi = 450)
 
 summaries[summaries$IC != 0,]
 
-ggplot(summaries, aes(x = IC, y = AdiffS, color = group, fill = group))+geom_jitter(alpha = 0.6)+theme_tufte()+geom_smooth(alpha = 0.2, method = "lm")+xlim(c(0,20))+ylim(c(-2,2))
+ggplot(summaries, aes(x = IC, y = AdiffS, color = group, fill = group))+geom_jitter(alpha = 0.6)+theme_tufte()+geom_smooth(alpha = 0.2, method = "lm")+xlim(c(0,20))+ylim(c(-1,1))
+
+
+ggplot(summaries, aes(x = IC, y = Adiff, color = group, fill = group))+geom_jitter(alpha = 0.6)+theme_tufte()+geom_smooth(alpha = 0.2, method = "lm")+xlim(c(0,20))+ylim(c(-100,100))
 
 
 

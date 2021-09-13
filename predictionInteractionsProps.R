62be534e90f45ac007e9c6da05d32f82c6e6a1a2
rfl-urbaniak <rfl-urbaniak@github.com>
2021-09-13 17:54:12 +0200

    comparing models

--- predictionInteractionsProps.R
+++ predictionInteractionsProps.R
@@ -17,6 +17,27 @@ ggplot()+geom_histogram(aes(x=summaries$AdiffS))
 noChange <-(0 - mean(summaries$Adiff))/sd(summaries$Adiff)
 averageChange <- mean(summaries$Adiff[summaries$group == "control"])
 
+# FinalHMC <- ulam(
+#   alist(
+#     AdiffS ~ dnorm( mu, sigma ),
+#     mu <- a + bADS[groupID] * ADS + 
+#     bIT[groupID] + bIC[groupID] * IC + 
+#       bADSIC * ADS * IC+ bCBS[groupID] *CBS,
+#     a ~ dnorm (0,0.3),
+#     bADS[groupID] ~ dnorm(0,.3),
+#     bADSIC ~ dnorm(0,.3),
+#     bCBS[groupID] ~ dnorm(0,.3),
+#     bIT[groupID] ~ dnorm(0,.3),
+#     bIC[groupID] ~ dnorm(0,.3),
+#     sigma  ~ dexp(1)
+#   ),
+#   data = summaries
+# )
+
+# saveRDS(FinalHMC, file = "models/FinalHMC.rds")
+
+FinalHMC <- readRDS(file = "models/FinalHMC.rds")
+
 
 # InteractionsModelDiff <- ulam(
 #   alist(
@@ -151,25 +172,58 @@ ggsave("images/priorsIC03.pdf", width = 20, height = 15, units = "cm", dpi = 450
 
 #now moving to posteriors, first by group
 #first by group for ADS = CBS = 0
+
+model <- FinalHMC
 ADS <- 0
 CBS <- 0
+xmin <-  2
+ymax <- -3
+
+visGroup <- function (model, ADS, CBS, xmin =2, ymax = -3)
+{
 groupID <- 1:3
 IC <- 5 
 data <- expand.grid(ADS = ADS,groupID = groupID, CBS = CBS, IC =  IC)
-data
-posterior <- extract.samples(InteractionsModelDiff, n = 1e5)
-mu <- link( InteractionsModelDiff, data=data ) 
-head(mu)
+posterior <- extract.samples(model, n = 1e5)
+mu <- link( model, data=data ) 
 colnames(mu) <- levels(summaries$group)
 muLong <- melt(mu)
 colnames(muLong) <- c("id", "group", "AdiffS")
-head(muLong)
-means <-  apply(mu , 2 , mean )
+means <-  round(apply(mu , 2 , mean ), 2)
+mu_HPDI <- round(apply( mu , 2 , HPDI ),2)
 means <- as.data.frame(means)
 means$group <- rownames(means)
 rownames(means) <- NULL
+meansDisp <- cbind(means,t(as.data.frame(mu_HPDI)))
+meansDisp <- meansDisp[,c(1,3,4)]
 
-ggplot(muLong)+geom_violin(aes(x = group, y = AdiffS, fill = group, color = group), alpha = 0.2)+theme_tufte()+xlab("")+labs(title = "Posterior dstribution of attack change", subtitle = "(at ADS = CBS = 0, IC at mean = 5)")+  geom_hline(data = means, aes(yintercept = means, colour = group), size = 0.3)+xlab("")+ylab("change in attacks (standardized)") +geom_hline(yintercept = noChange, size = .3)+annotate(geom = "text", label = "no change in count", x = "empathy", y = 0.65, hjust = 0, vjust = 0, size = 4) 
+plot <- ggplot(muLong)+geom_violin(aes(x = group, y = AdiffS), alpha = 0.2)+
+  xlab("")+
+  labs(title = paste("ADS=", ADS, ", CBS=",  CBS, ", IC=5", sep = ""))+
+  theme_tufte()+ 
+  annotation_custom(tableGrob(meansDisp), xmin=xmin,  ymax=ymax)
+return(plot)
+}
+
+
+visGroupA2C_2 <- visGroup(model = FinalHMC, ADS = 2,CBS = -2 , ymax = -2)
+visGroupA2C0 <- visGroup(model = FinalHMC, ADS = 2,CBS = 0 )
+visGroupA2C2 <- visGroup(model = FinalHMC, ADS = 2,CBS = 2, ymax = -3.5 )
+
+visGroupA0C_2 <- visGroup(model = FinalHMC, ADS = 0,CBS = -2 , ymax = -2)
+visGroupA0C0 <- visGroup(model = FinalHMC, ADS = 0,CBS = 0 )
+visGroupA0C2 <-  visGroup(model = FinalHMC, ADS = 0,CBS = 2, ymax = -4 )
+
+visGroupA2C_2 <-  visGroup(model = FinalHMC, ADS = 2,CBS = -2 , ymax = -2)
+visGroupA2C0 <- visGroup(model = FinalHMC, ADS = 2,CBS = 0 )
+visGroupA2C2 <- visGroup(model = FinalHMC, ADS = 2,CBS = 2, ymax = -4 )
+
+ggarrange(visGroupA2C_2 , visGroupA2C0,visGroupA2C2, 
+          visGroupA0C_2, visGroupA0C0, visGroupA0C2,
+          visGroupA2C_2, visGroupA2C0, visGroupA2C2, ncol =3, nrow = 3)
+
+
+
 
 
 ggsave("images/predictedGroups0.pdf", width = 20, height = 15, units = "cm", dpi = 450)                             
